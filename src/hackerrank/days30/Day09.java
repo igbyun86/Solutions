1a1f5e89bdcbcb498c6361844ad8b9974fbf7131
big <big@big-PC>
2019-03-20 13:02:53 +0900

    day09 commit

---
+++ src/hackerrank/days30/Day09.java
@@ -0,0 +1,37 @@
+package hackerrank.days30;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Day09 {
+	
+	static int factorial(int n) {
+		int result = 1;
+		
+		for (int i = 1; i <= n; i++) {
+			result = i * result;
+		}
+		
+		return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = factorial(n);
+
+        System.out.println(String.valueOf(result));
+        
+        scanner.close();
+    }
+    
+}
