e62c73c1d68d4c0a42c0b3e9c041f032c9f1f7ad
big <big@big-PC>
2019-04-18 17:26:35 +0900

    day22 commit

--- src/hackerrank/days30/day22/Solution.java
+++ src/hackerrank/days30/day22/Solution.java
@@ -1,71 +1,49 @@
 package hackerrank.days30.day22;
 
+
 import java.util.*;
 import java.io.*;
-
 class Node{
     Node left,right;
     int data;
     Node(int data){
-        this.data=data;
-        left=right=null;
+        this.data = data;
+        left=right = null;
     }
 }
+class Solution{
 
-public class Solution {
-
+	
 	public static int getHeight(Node root){
-		int height = 0;
-		int left = 0;
-		int right = 0;
-		
-		Node rightNode = root.right;
-		Node leftNode = root.left;
-		
-		while(rightNode != null){
-			rightNode = rightNode.right;
-			if(rightNode !=null){
-				System.out.println(rightNode.data);
-			}
-			right++;
-		}
-		
-		while(leftNode != null){
-			leftNode = leftNode.right;
-			if(leftNode !=null){
-				System.out.println(leftNode.data);
-			}
-			left++;
-		}
-		
-		//System.out.println(right);
-		//System.out.println(left);
-		
-		if(right > left) height = right;
-		else height = left;
-		
-		return height;
+		if(root == null){
+	    	return -1;
+	    }
+	    else{
+	    	System.out.println(1 + Math.max(getHeight(root.left), getHeight(root.right)));
+	    	return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+	    }
     }
 
     public static Node insert(Node root,int data){
-        if(root==null){
-            return new Node(data);
+    	if(root == null){
+    		return new Node(data);
         }
         else{
             Node cur;
             if(data <= root.data){
-                cur = insert(root.left,data);
-                root.left=cur;
+                cur = insert(root.left, data);
+                root.left = cur;
             }
             else{
-                cur = insert(root.right,data);
-                root.right=cur;
+                cur = insert(root.right, data);
+                root.right = cur;
             }
             return root;
         }
     }
-	 public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
+    
+    public static void main(String args[]){
+    	Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         Node root = null;
         
