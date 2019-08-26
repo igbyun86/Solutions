579996df3999f3ca60510ff6c44c82cbd9c14e4a
igbyun86 <48421246+igbyun86@users.noreply.github.com>
2019-08-26 23:37:27 +0900

    Update Solution.java

--- src/hackerrank/days30/day02/Solution.java
+++ src/hackerrank/days30/day02/Solution.java
@@ -4,31 +4,21 @@ import java.util.Scanner;
 
 public class Solution {
 
-	static void solve(double meal_cost, int tip_percent, int tax_percent) {
-
-		double tip = meal_cost * tip_percent /100.0;
-		double tax = meal_cost * tax_percent /100.0;
-		double total = meal_cost + tip + tax;   
-		
-		int totalCost = (int) Math.round(total);
-		
-		System.out.println(totalCost);
-    }
-
 
     public static void main(String[] args) {
-    	Scanner scanner = new Scanner(System.in);
-        double meal_cost = scanner.nextDouble();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int tip_percent = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int tax_percent = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        solve(meal_cost, tip_percent, tax_percent);
-
-        scanner.close();
+        Scanner sc = new Scanner(System.in);
+       
+        double mealCost = sc.nextDouble();
+        int tipPercent = sc.nextInt();
+        int taxPercent = sc.nextInt();
+        
+        double tip = (double) (mealCost * tipPercent / 100.0);
+        double tax = (double) (mealCost * taxPercent / 100.0);
+        
+        int totalCoas = (int) Math.round(mealCost + tip + tax);
+        
+        System.out.println(totalCoas);
+        
+        sc.close();
     }
 }
